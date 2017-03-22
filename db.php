1499b247a1ff0d7916fb0b9a650cff95364c2f94
Sutida58160436 <58160436@go.buu.ac.th>
2017-03-22 21:01:55 +0700

    lab07

---
+++ db.php
@@ -0,0 +1,10 @@
+
+<?
+$dbhost = 'localhost';
+$dbuser = 'it58160436';
+$passwd = '087706';
+$dbname = 'it58160436';
+$conn = new mysqli("$dbhost","$dbuser","$passwd","$dbname");
+$conn->query('SET NAMES UTF8'); 
+?>
+
